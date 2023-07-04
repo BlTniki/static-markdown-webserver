d5a1eab9bef0fe0f1b3be3614a13ad6f70b77f6c
BITniki <shakarimovdima@ya.ru>
2023-07-04 14:13:39 +0300

    added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_commonmark import Commonmark
 from flask_debugtoolbar import DebugToolbarExtension
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 from config import Config
 
@@ -9,4 +12,29 @@ app.config.from_object(Config)
 cm = Commonmark(app)
 toolbar = DebugToolbarExtension(app)
 
-from app import routes
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+# setup rotating INFO logging
+file_handler = RotatingFileHandler('logs/info.log', maxBytes=10240,
+                                   backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('App startup')
+
+# setup rotating ERROR logging
+file_handler = RotatingFileHandler('logs/error.log', maxBytes=10240,
+                                   backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.ERROR)
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.ERROR)
+
+
+from app import routes, error_handler
