9553ddb4c1859b83c6fad2d9374e3296263a7f86
BITniki <shakarimovdima@ya.ru>
2023-07-11 15:47:41 +0300

    fix reading env var

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 class Config(object):
@@ -10,21 +11,14 @@ class Config(object):
     CONFIG_MSG = "ABS!!"
 
 
-class DevConfig(Config):
-    DEBUG = os.environ.get("DEBUG") or True
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    PATH_TO_VAULT = os.environ.get("PATH_TO_VAULT") or os.path.abspath(r"app/md_static/")
-    OFFICIAL_EXTENSIONS_TO_USE = os.environ.get("OFFICIAL_EXTENSIONS_TO_USE") or ['fenced_code', 'tables', 'nl2br']
-    OFFICIAL_EXTENSIONS_CONFIG = os.environ.get("OFFICIAL_EXTENSIONS_TO_USE") or {}
-    CONFIG_MSG = "DEV!!"
-
-
 class ProdConfig(Config):
-    DEBUG = os.environ.get("DEBUG") or False
+    DEBUG = os.environ.get("DEBUG") == "True"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     PATH_TO_VAULT = os.environ.get("PATH_TO_VAULT") or os.path.abspath(r"app/md_static/")
-    OFFICIAL_EXTENSIONS_TO_USE = os.environ.get("OFFICIAL_EXTENSIONS_TO_USE") or ['fenced_code', 'tables', 'nl2br']
-    OFFICIAL_EXTENSIONS_CONFIG = os.environ.get("OFFICIAL_EXTENSIONS_TO_USE") or {}
+    OFFICIAL_EXTENSIONS_TO_USE = json.loads(os.environ.get("OFFICIAL_EXTENSIONS_TO_USE")) if os.environ.get(
+        "OFFICIAL_EXTENSIONS_TO_USE") else ['fenced_code', 'tables', 'nl2br']
+    OFFICIAL_EXTENSIONS_CONFIG = json.loads(os.environ.get("OFFICIAL_EXTENSIONS_CONFIG")) if os.environ.get(
+        "OFFICIAL_EXTENSIONS_CONFIG") else {}
     CONFIG_MSG = "PROD!!"
 
 
