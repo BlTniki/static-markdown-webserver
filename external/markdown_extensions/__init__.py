d925c9c8e7fa391826dae0c98fe119cedb701d5d
BITniki <shakarimovdima@ya.ru>
2024-05-21 11:10:12 +0300

    lol

--- external/markdown_extensions/__init__.py
+++ external/markdown_extensions/__init__.py
@@ -1,2 +1,6 @@
+from .markdown_strikethrough.markdown_strikethrough import MarkdownStrikethrough
+from .markdown_mark.markdown_mark import MarkdownMark
+from .markdown_obsidian_katex import makeExtension as katexMakeExtension
+
 # init list with unofficial extensions entities
-unof_extensions = []
+unof_extensions = [MarkdownStrikethrough(), MarkdownMark(), katexMakeExtension()]
